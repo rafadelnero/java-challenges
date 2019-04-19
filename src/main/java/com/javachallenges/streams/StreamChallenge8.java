ba95debe18422f7ef113482a151030b0bac8ead3
rafadelnero <rafacdelnero@gmail.com>
2019-04-19 12:36:31 +0200

    Upgraded challenges

--- src/main/java/com/javachallenges/streams/StreamChallenge8.java
+++ src/main/java/com/javachallenges/streams/StreamChallenge8.java
@@ -5,12 +5,12 @@ import java.util.stream.IntStream;
 public class StreamChallenge8 {
 
     public static void main(String... doYourBest) {
-        IntStream.iterate(0, i -> i + 2)
+        IntStream.iterate(10, i -> i - 2)
                 .limit(5)
                 .skip(1)
+                .dropWhile(i -> i < 6)
                 .sorted()
-                .takeWhile(i -> i < 10)
-                .dropWhile(i -> i > 2) // Removes the elements if the first condition matches
+                .takeWhile(i -> i > 2)
                 .forEach(System.out::println);
     }
 
