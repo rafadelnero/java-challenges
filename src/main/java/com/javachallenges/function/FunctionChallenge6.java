4240e5a0c6b2b5bb7ef054eee2b92388e2d0ed70
rafaeldelnero <rafael.delnero@fabfitfun.com>
2021-02-23 07:55:50 +0000

    - Removed duplicate challenge. - Removed unused break feature.

--- src/main/java/com/javachallenges/function/FunctionChallenge6.java
+++ src/main/java/com/javachallenges/function/FunctionChallenge6.java
@@ -12,11 +12,12 @@ public class FunctionChallenge6 {
         Function<Integer, Integer> sub = (a) -> a - 2;
         Function<Integer, Integer> mult = (a) -> a * 3;
 
+        BiFunction<Integer, Integer, Integer> func =
+            add.andThen(sub).andThen(sub).andThen(sub);
+
         BiConsumer<Integer, Integer> consumer = (a, b)
                 -> System.out.println(a + 2 + b + 2);
 
-        BiFunction<Integer, Integer, Integer> func =
-                add.andThen(sub).andThen(sub).andThen(sub);
         int firstResult = func.apply(2, 2);
 
         add.andThen(sub).andThen(mult);
