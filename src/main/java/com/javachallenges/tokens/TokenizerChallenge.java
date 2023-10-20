f294a75d09d8e802a33ea0f010bab22db582c78a
rafadelnero <R@fa4Java222>
2023-10-20 13:20:15 +0200

    - Fixed pom file.

--- src/main/java/com/javachallenges/tokens/TokenizerChallenge.java
+++ src/main/java/com/javachallenges/tokens/TokenizerChallenge.java
@@ -6,15 +6,15 @@ import java.util.regex.Pattern;
 public class TokenizerChallenge {
 
     public static void main(String[] args) throws Exception {
-	Scanner sc = new Scanner("ThisIsIt,theFinalString,NoBugsProject");
-	// See the result on nobugsproject.com
-	Pattern pattern = Pattern.compile("[^\\w*]");
-	sc.useDelimiter(pattern);
-	// Make your code powerful, make your best
-	while (sc.hasNext()) {
-	    System.out.println(sc.next());
-	}
+			Scanner sc = new Scanner("ThisIsIt,theFinalString,NoBugsProject");
+			// See the result on nobugsproject.com
+			Pattern pattern = Pattern.compile("[^\\w*]");
+			sc.useDelimiter(pattern);
+			// Make your code powerful, make your best
+			while (sc.hasNext()) {
+				System.out.println(sc.next());
+			}
 
-	sc.close();
+			sc.close();
     }
 }
