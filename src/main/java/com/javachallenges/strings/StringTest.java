ec7fde679cdc95dda1d3104d429a4e4e31b9609e
rafadelnero <R@fa4Java222>
2023-10-20 13:11:12 +0200

    - Sync Git

--- src/main/java/com/javachallenges/strings/StringTest.java
+++ src/main/java/com/javachallenges/strings/StringTest.java
@@ -1,17 +1,16 @@
 package com.javachallenges.strings;
 
 public class StringTest {
-    
-    /**
-     * How many objects are going to be created?
-     */
-    public static void main(String[] args) {
-	String batman = "Batman";
-	String robin = "Robin";
-	String joker = "Joker";
-	String anotherJoker = "Joker";
-	
-	System.out.println(batman + robin + joker + anotherJoker);
-    }
+
+	public static void main(String[] args) {
+	  String batman = "Batman";
+	  String robin = "Robin";
+	  String joker = "Joker";
+	  String anotherJoker = "Joker";
+
+		System.out.println(batman + robin + joker + anotherJoker);
+	}
     
 }
+
+
